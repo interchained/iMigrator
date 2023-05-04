5fc856fc6c9f3b9b9dec419bcf11464e0fffa31c
⛓️INTERCHAINED ⚡Electronero Network ʕ•̫͡•ʔ-̫͡-ʕ•͓͡•ʔ-̫͡-ʕ•̫͡•ʔ-̫͡-ʔ <evansmark.work@gmail.com>
2023-05-04 18:20:22 -0400

    Create FREN_iMigrator.sol

---
+++ FREN_iMigrator.sol
@@ -0,0 +1,167 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.5;
+
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+/**
+ *                                         ...........
+ *                                 .::--==================--:..
+ *                            .:-===++*#%%@@@@@@@@@@@@@%%##*+===--:.
+ *                        .:-==+*#%@@@@@@@@@@@@@@@@@@@@@@@@@@@@%#*===-:
+ *                     .:-==*#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%#+==-:
+ *                   :-==*%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#+==-.
+ *                 :==+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#==-.
+ *               :==+%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#+=-.
+ *             .==+%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*=------=+#@@@@@@@@@@@@@@@#==-
+ *            -==#@@@@@@@@@@@@@@@@@@@@%#####%@@@@@#-------------#@@@@@@@@@@@@@@@*==:
+ *          .==+@@@@@@@@@@@@@@@@@@%+-:::::::--+#@@---------------=@@@@@@@@@@@@@@@%==-
+ *         .==*@@@@@@@@@@@@@@@@@%=:::::::::------*%---------------#@@@@@@@@@@@@@@@@+=-
+ *        :==#@@@@@@@@@@@@@@@@@*-:::::----------------------=+#######%@@@@@@@@@@@@@@*==.
+ *       .==#@@@@@@@@@@@@@@@@@+---------------------------*%#+---------+%@@@@@@@@@@@@*==
+ *      .==*@@@@@@@@@@@@@@@@@*----------------------------=--------------=@@@@@@@@@@@@+=-
+ *      -=+@@@@@@@@@@@@@@@@@%-----------=+****+=--------------------++**#**@@@@@@@@@@@%==:
+ *     :==%@@@@@@@@@@@@@@@@@*-------=###++====+*%%*-------------+#@%*+==--=+%@@@@@@@@@@#==
+ *     ==*@@@@@@@@@@@@@@@@@@=-----=%#=------------*@*---------*@#+----------=@@@@@@@@@@@==:
+ *    .==%@@@@@@@@@@@@@@@@@@------=-----------------#%------=@#=------------=@@@@@@@@@@@*==
+ *    :==@@@@@@@@@@@@@@@@@%+-------------------=+***+%%-----#*--------------#@@@@@@@@@@@%==.
+ *    -=+@@@@@@@@@@@@@@@@+---------------=+#%@%#*+===+@=-------------------#@@@@@@@@@@@@@==:
+ *    -=*@@@@@@@@@@@@@@%===-----------=*%@%*=---------=#---==-----------=*@@@@@@@@@@@@@@@==:
+ *    -=*@@@@@@@@@@@@@%======-------=#@@*=------------=#----+**++++++*###++%@@@@@@@@@@@@@==:
+ *    -=+@@@@@@@@@@@@@=========----#@@+--------------=%=-------=++++*%*----#@@@@@@@@@@@@@==:
+ *    :==@@@@@@@@@@@@#===========--+*=--------------*%=----------=**+---=*%@@@@@@@@@@@@@%==.
+ *    .==%@@@@@@@@@@@#==============-------------+#%+-------=+***+-=+*#**+%@@@@@@@@@@@@@*==
+ *     ==*@@@@@@@@@@@%================-----=+*###*=----=+**###*****#%#===%@@@@@@@@@@@@@@==:
+ *     :==%@@@@@@@@@@@+==================-=*++=--=+**##*##**+====*#*===+@@@@@@@@@@@@@@@#==
+ *      -=+@@@@@@@@@@@@+===================+*####*++==*#+=====*#*=====#@@@@@@@@@@@@@@@%==:
+ *      .==*@@@@@@@@@@@@#============+*###*++=====+*##+==+*##*=====+#@@@@@@@@@@@@@@@@@+=-
+ *       .==#@@@@@@@@@@@@@*==========@+=+#########*++*###*+=====+#@@@@@@@@@@@@@@@@@@@+=-
+ *        :==#@@@@@@@@@@@@@@%*=======+#############**+======*#%@@@@@@@@@@@@@@@@@@@@@+==
+ *         .==*@@@@@@@@@@@@@@@@%#*+===================++*%@@@@@@@@@@@@@@@@@@@@@@@@@+=-
+ *          .-=+@@@@@@@@@@@@@@@@@@@@@%%#####*#####%%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%==-
+ *            -==#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*==:
+ *             .-=+%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#==-
+ *               :==+%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#==-.
+ *                 :==+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%*==-.
+ *                   .-==*%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#+==-.
+ *                      :-==*#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%#+==-.
+ *                         :-===*#%@@@@@@@@@@@@@@@@@@@@@@@@@@@@%#+===-.
+ *                            .:-====+*##%%@@@@@@@@@@@@%%##*+===-::.
+ *                                 .::--==================--:..
+ *                                          ..........
+ */
+
+/**▪  ▄.▄.▄▄▄▄.▪▄▄▄▄.▄▄▄ ▄▄▄· ▄▄.·▄ ▄▄▄▄· ▪  ▄  ▄·▄▄▄▄ .·▄▄▄▄
+ *██ •█▌█▌•██ ·▀▀▄. ▀▄▄█·▐█ ▌▪██▪▐█ █▀▀█ ██ •█▌▐█ ▀▀▄▪ ▐█▪ ██
+ *▐█·▐█▐▐▌ ▐█.▪▐▀▀▪▄▐▀▀▄ ██ ▄▄██▀▐█ █▀▀█·▐█·▐█▐▐▌▐▀▀▪▄·▐█· ██
+ *▐█·██▐█▌ ▐█▌·▐█▄▄▌▐█•█▌▐███▌██ ▐█ █▪ █·▐█ ██▐█▌▐█▄▄▌·▐█. ██
+ * █▪ ▀▪▀ •▀▀▀ .▀▀▀▀·▀ ▀• ▀▀▀· ▀ •▀ ▀• ▀  █▪ ▀ ▀ •▀▀▀▀  ▀▀▀▀▀
+ *
+ */
+contract FRENCHAIN_iMigrator is Context, Ownable {
+    using SafeERC20 for IERC20;
+
+    bool private burn;
+    bool public maintenance;
+
+    uint256 public version_index;
+
+    address payable support;
+    address payable deployers;
+
+    mapping(address => bool) internal blocklist;
+    mapping(uint256 => address) public versions;
+
+    constructor(
+        address payable _deployers,
+        address payable _support,
+        address payable _frenchain_v1,
+        address payable _frenchain_v2
+    ) Ownable() {
+        support = _support;
+        deployers = _deployers;
+        require(_newVersion(_frenchain_v1));
+        require(_newVersion(_frenchain_v2));
+        transferOwnership(_deployers);
+        emit OwnershipTransferred(address(0), owner());
+    }
+
+    receive() external payable {}
+
+    fallback() external payable {}
+
+    function version(uint256 index)
+        public
+        view
+        virtual
+        returns (address payable)
+    {
+        require(uint256(index) > uint256(0));
+        return payable(versions[index]);
+    }
+
+    function _newVersion(address _addressToMap) public virtual onlyOwner returns(bool) {
+        version_index++;
+        versions[version_index] = _addressToMap;
+        return true;
+    }
+
+    function _burnable(bool isBurnable) public virtual onlyOwner {
+        burn = isBurnable;
+    }
+
+    function migrate(uint256 amount) public payable virtual {
+        require(!maintenance);
+        require(!blocklist[_msgSender()]);
+        IERC20 v1_token = IERC20(version(version_index - 1)); // should always target previous version
+        IERC20 v2_token = IERC20(version(version_index)); // latest version
+        uint256 allowed = v1_token.allowance(_msgSender(), address(this));
+        uint256 reserves = v2_token.balanceOf(address(this));
+        require(uint256(allowed) >= uint256(amount), "Increase allowance");
+        require(uint256(reserves) >= uint256(amount), "Not enough reserves");
+        uint256 balanceBefore = v1_token.balanceOf(address(this));
+        v1_token.safeTransferFrom(
+            payable(_msgSender()),
+            payable(address(this)),
+            amount
+        );
+        uint256 balanceAfter = v1_token.balanceOf(address(this));
+        require(
+            uint256(balanceAfter) >= uint256(balanceBefore) + uint256(amount)
+        );
+        if (burn) {
+            v1_token.safeTransfer(payable(address(0)), amount);
+        }
+        v2_token.safeTransfer(payable(_msgSender()), amount);
+        if (uint256(msg.value) > uint256(0)) {
+            (bool sent_eth, ) = support.call{value: msg.value}("");
+            require(sent_eth);
+        }
+    }
+
+    function _maintenance(bool isPaused) public virtual onlyOwner {
+        maintenance = isPaused;
+    }
+
+    function _block(address _addressToBan) public virtual onlyOwner {
+        blocklist[_addressToBan] = true;
+    }
+
+    function blocked(address _wallet) public view virtual returns (bool) {
+        return blocklist[_wallet];
+    }
+
+    function _emergencyWithdraw(bool isEth, IERC20 token)
+        public
+        virtual
+        onlyOwner
+    {
+        if (!isEth) {
+            token.safeTransfer(support, token.balanceOf(address(this)));
+        } else {
+            (bool sent_eth, ) = support.call{value: address(this).balance}("");
+            require(sent_eth);
+        }
+    }
+}
